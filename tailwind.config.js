85a46d1c72c2ce3f1e5f17d1e0bbad130f9bb349
Kaori Kawakami <k.kawakami08@gmail.com>
2023-04-04 21:23:20 -0700

    initial commit and theme styling config for tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryCyan: "hsl(var(--primary-cyan))",
+        darkCyan: "hsl(var(--dark-cyan))",
+        darkGrayishCyan: "hsl(var(--dark-grayish-cyan))",
+        grayishCyan: "hsl(var(--grayish-cyan))",
+        lightGrayishCyan: "hsl(var(--light-grayish-cyan))",
+        veryLightGrayishCyan: "hsl(var(--very-light-grayish-cyan))",
+      },
+      fontFamily: {
+        sans: ["Space Mono", "Arial", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
